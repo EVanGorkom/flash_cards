5ae738b11b97a217ea76e90a9eb6e575bdc4da59
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-07-28 12:25:37 -0500

    Create the turn class and corresponding test file

---
+++ lib/turn.rb
@@ -0,0 +1,9 @@
+
+class Turn
+  def initialize(guess, card)
+    @guess = guess
+    @card = guess
+  end
+
+  
+end
---
+++ spec/turn_spec.rb
@@ -0,0 +1,39 @@
+require './lib/card'
+require './lib/turn'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it "has a card" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to be_instance_of(Card)
+  end
+
+  it "has a user's guess" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  it "can tell if the guess was correct" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.correct?).to be true
+  end
+
+  it "can return feedback about a guess" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.feedback).to eq("Correct!")
+  end
+end
